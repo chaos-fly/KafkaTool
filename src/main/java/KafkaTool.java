e5fae6996a2c1d7de9335b1ea11239e9e1ce7553
grj <grj@yy.com>
2017-06-23 18:57:43 +0800

    build ok

--- src/main/java/KafkaTool.java
+++ src/main/java/KafkaTool.java
@@ -1,117 +1,137 @@
-import kafka.api.*;
-import kafka.cluster.BrokerEndPoint;
-import kafka.common.ErrorMapping;
-import kafka.common.OffsetAndMetadata;
-import kafka.common.OffsetMetadata;
-import kafka.common.TopicAndPartition;
-import kafka.network.BlockingChannel;
-import scala.Option;
-import scala.collection.immutable.HashMap;
-import scala.collection.immutable.Map;
+import org.apache.kafka.clients.KafkaClient;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
 
-import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-class KafkaTool {
-    public static void main(String[] args) {
-        System.out.println("kafka");
-        BlockingChannel channel = new BlockingChannel("106.38.255.199", 9092,
-            BlockingChannel.UseDefaultBufferSize(),
-            BlockingChannel.UseDefaultBufferSize(),
-            5000 /* read timeout in millis */);
-        channel.connect();
+public class KafkaTool {
 
-        final String MY_GROUP = "test_kafka_test_topic_1";
-        final String MY_CLIENTID = "demoClientId";
-        int correlationId = 0;
-        final TopicAndPartition testPartition0 = new TopicAndPartition("test_topic_1", 0);
-        final TopicAndPartition testPartition1 = new TopicAndPartition("test_topic_1", 1);
+    private String mGroup;
+    private String mTopic;
+    private KafkaConsumer<String, String> mConsumer;
 
-        channel.send(new GroupCoordinatorRequest(
-            MY_GROUP,
-            GroupCoordinatorRequest.CurrentVersion(),
-            correlationId++,
-            MY_CLIENTID));
-        GroupCoordinatorResponse metadataResponse = GroupCoordinatorResponse.readFrom(channel.receive().payload());
+    public KafkaTool(String host, String topic, String group) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", host);
+        props.put("zk.connectiontimeout.ms", "5000000");
+        props.put("group.id", group);
+        props.put("enable.auto.commit", "false");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-        if (metadataResponse.errorCode() == ErrorMapping.NoError()) {
+        mGroup = group;
+        mTopic = topic;
+        mConsumer = new KafkaConsumer<>(props);
+    }
 
-            Option<BrokerEndPoint> coordinatorOpt = metadataResponse.coordinatorOpt();
-            BrokerEndPoint endPoint = coordinatorOpt.get();
-
-            System.out.printf("host:%s port:%d", endPoint.host(), endPoint.port());
-            if (endPoint.host() != channel.host() || endPoint.port() != channel.port()) {
-                // 连接到管理offset的broker
-                channel.disconnect();
-                channel = new BlockingChannel(endPoint.host(), endPoint.port(),
-                    BlockingChannel.UseDefaultBufferSize(),
-                    BlockingChannel.UseDefaultBufferSize(),
-                    5000 /* read timeout in millis */);
-                channel.connect();
-            }
-        } else {
-            // retry (after backoff)
+    public void TestAssign() {
+        // 自行balance partition，不能和subscribe混用
+        // offset需要自己保存，开始的时候通过seek指定开始消费的位置
+        List<TopicPartition> topicPartitions = new ArrayList<>();
+        List<PartitionInfo> partitionInfos = mConsumer.partitionsFor(mTopic);
+        for (PartitionInfo p : partitionInfos) {
+            //System.out.println(p);
+            topicPartitions.add(new TopicPartition(mTopic, p.partition()));
         }
 
-        // How to commit offsets
+        mConsumer.assign(topicPartitions);
+        //mConsumer.seekToBeginning(topicPartitions);
+        for (TopicPartition p : topicPartitions) {
+            // 最新的offset
+            long position = mConsumer.position(p);
+            System.out.printf("partion:%d position:%s%n", p.partition(), position);
 
-        long now = System.currentTimeMillis();
-        Map<TopicAndPartition, OffsetAndMetadata> offsets = new HashMap<>();
-        /*
-        offsets.put(testPartition1, new OffsetAndMetadata(
-            new OffsetMetadata(200L, "more metadata"),
-            now,
-            now + 1000 * 1000 * 5));
-        //offsets.put(testPartition1, new OffsetAndMetadata(200L, "more metadata"));
-        OffsetCommitRequest commitRequest = new OffsetCommitRequest(
-            MY_GROUP,
-            offsets,
-            (short)1,
-            correlationId++,
-            MY_CLIENTID);
-        channel.send(commitRequest);
-        OffsetCommitResponse commitResponse = OffsetCommitResponse.readFrom(channel.receive().payload());
-        if (commitResponse.hasError()) {
-            for (partitionErrorCode : commitResponse.error()) {
-                if (partitionErrorCode == ErrorMapping.OffsetMetadataTooLargeCode()) {
-                    // You must reduce the size of the metadata if you wish to retry
-                } else if (partitionErrorCode == ErrorMapping.NotCoordinatorForConsumerCode() || partitionErrorCode == ErrorMapping.ConsumerCoordinatorNotAvailableCode()) {
-                    channel.disconnect();
-                    // Go to step 1 (offset manager has moved) and then retry the commit to the new offset manager
-                } else {
-                    // log and retry the commit
+            mConsumer.seek(p, position - 10L);
+        }
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        while (true) {
+            ConsumerRecords<String, String> records = mConsumer.poll(500);
+            for (TopicPartition partition : records.partitions()) {
+                List<ConsumerRecord<String, String>> partionRecords = records.records(partition);
+                for (ConsumerRecord<String, String> record : partionRecords) {
+                    System.out.printf("partition:%d offset:%d value:%s time:%s %d%n",
+                        partition.partition(), record.offset(), record.value(),
+                        format.format(new Date(record.timestamp() * 1000)), record.timestamp());
                 }
-            }
-        }
-    */
-        /*
-        // How to fetch offsets
 
-        List<TopicAndPartition> partitions = new ArrayList<TopicAndPartition>();
-        partitions.add(testPartition0);
-        OffsetFetchRequest fetchRequest = new OffsetFetchRequest(
-            MY_GROUP,
-            partitions,
-            (short) 1 , // version 1 and above fetch from Kafka, version 0 fetches from ZooKeeper
-            correlationId,
-            MY_CLIENTID);
-        try {
-            channel.send(fetchRequest.underlying());
-            OffsetFetchResponse fetchResponse = OffsetFetchResponse.readFrom(channel.receive().buffer());
-            OffsetMetadataAndError result = fetchResponse.offsets().get(testPartition0);
-            short offsetFetchErrorCode = result.error();
-            if (offsetFetchErrorCode == ErrorMapping.NotCoordinatorForConsumerCode()) {
-                channel.disconnect();
-                // Go to step 1 and retry the offset fetch
-            } else if (errorCode == ErrorMapping.OffsetsLoadInProgress()) {
-                // retry the offset fetch (after backoff)
-            } else {
-                long retrievedOffset = result.offset();
-                String retrievedMetadata = result.metadata();
+                // save offset by self
             }
-        } catch (IOException e) {
-            channel.disconnect();
-            // Go to step 1 and then retry offset fetch after backoff
         }
-        */
+    }
+
+    public void ResetConsumerGroupOffset(Map<Integer, Long> partition2Offset) {
+        mConsumer.subscribe(Arrays.asList(mTopic));
+
+        List<TopicPartition> topicPartitions = new ArrayList<>();
+        for (Map.Entry<Integer, Long> entry : partition2Offset.entrySet()) {
+            topicPartitions.add(new TopicPartition(mTopic, entry.getKey()));
+        }
+
+        // 该分区最旧的记录
+        System.out.println("==== 开始校验每个分区最旧的消息的offset ====");
+        System.out.println(topicPartitions);
+        Map<TopicPartition, Long> beginOffset = mConsumer.beginningOffsets(topicPartitions);
+        for (Map.Entry<TopicPartition, Long> entry : beginOffset.entrySet()) {
+            Integer p = entry.getKey().partition();
+            if (entry.getValue().compareTo(partition2Offset.get(p)) > 0) {
+                System.out.printf("Error. partition:%d min offset is %d %n", p, entry.getValue());
+                return;
+            }
+            System.out.printf("partition:%d oldest offset:%d%n", p, entry.getValue());
+        }
+
+        // 该分区最新的记录
+        System.out.println("==== 开始校验每个分区最新的消息的offset ====");
+        Map<TopicPartition, Long> endOffset = mConsumer.endOffsets(topicPartitions);
+        for (Map.Entry<TopicPartition, Long> entry : endOffset.entrySet()) {
+            Integer p = entry.getKey().partition();
+            if (entry.getValue().compareTo(partition2Offset.get(p)) < 0) {
+                System.out.printf("Error. partition:%d max offset is %d %n", p, entry.getValue());
+                return;
+            }
+            System.out.printf("partition:%d newest offset:%d%n", p, entry.getValue());
+        }
+
+        System.out.println("==== 开始重置offset，每个分区至少要有一条未消费记录 ====");
+        Set<Integer> allPatitions = partition2Offset.keySet();
+        int retry = 3;
+        while (allPatitions.size() > 0) {
+            ConsumerRecords<String, String> records = mConsumer.poll(3000);
+            for (TopicPartition partition : records.partitions()) {
+                if (!allPatitions.contains(partition.partition())) {
+                    continue;
+                }
+
+                long offset = partition2Offset.get(partition.partition());
+                mConsumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(offset + 1)));
+                allPatitions.remove(partition.partition());
+                System.out.printf("Ok. set partition:%d offset to:%d %n", partition.partition(), offset);
+            }
+
+            if (records.isEmpty()) {
+                System.out.println("poll msg from topic timeout.");
+            }
+
+            retry --;
+            if (retry == 0) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("kafka tool");
+
+        KafkaTool tool = new KafkaTool("106.38.255.199:9092", "test_topic_1", "test_kafka_test_topic_1");
+
+        Map<Integer, Long> p2offset = new HashMap<>();
+        p2offset.put(0, 21897L);
+        p2offset.put(1, 1300L);
+        tool.ResetConsumerGroupOffset(p2offset);
+        //tool.TestAssign();
     }
 }
